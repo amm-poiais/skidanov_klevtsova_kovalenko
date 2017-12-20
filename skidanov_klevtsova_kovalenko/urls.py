ef35b9897412407bd0bd104ea59197bfb8f5b890
ReD_Sky <redxsky59@gmail.com>
2017-12-20 22:46:29 +0300

    Changed path to url

--- skidanov_klevtsova_kovalenko/urls.py
+++ skidanov_klevtsova_kovalenko/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from WitcherZeroPlayerGame import views as witcher_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', witcher_views.login),
-    path('register/', witcher_views.register),
-    path('create_witcher/', witcher_views.create_witcher)
+    url('admin/', admin.site.urls),
+    url('login/', witcher_views.login),
+    url('register/', witcher_views.register),
+    url('create_witcher/', witcher_views.create_witcher)
 ]
